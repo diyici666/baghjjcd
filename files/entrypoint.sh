2f7cb6f841c985a7f85ee15734ec9ab5307ff86c
diyici666 <73017871+diyici666@users.noreply.github.com>
2023-11-25 00:32:47 +0800

    Add files via upload

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # 设置各变量
-WSPATH=${WSPATH:-'arco'}
+WSPATH=${WSPATH:-'argo'}
 UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
 WEB_USERNAME=${WEB_USERNAME:-'admin'}
 WEB_PASSWORD=${WEB_PASSWORD:-'password'}
 
 generate_config() {
-  cat > /app/config.json << EOF
+  cat > config.json << EOF
 {
     "log":{
         "access":"/dev/null",
@@ -194,17 +194,23 @@ generate_config() {
             "tag":"WARP",
             "protocol":"wireguard",
             "settings":{
-                "secretKey":"cKE7LmCF61IhqqABGhvJ44jWXp8fKymcMAEVAzbDF2k=",
+                "secretKey":"YFYOAdbw1bKTHlNNi+aEjBM3BO7unuFC5rOkMRAz9XY=",
                 "address":[
                     "172.16.0.2/32",
-                    "fd01:5ca1:ab1e:823e:e094:eb1c:ff87:1fab/128"
+                    "2606:4700:110:8a36:df92:102a:9602:fa18/128"
                 ],
                 "peers":[
                     {
                         "publicKey":"bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
+                        "allowedIPs":[
+                            "0.0.0.0/0",
+                            "::/0"
+                        ],
                         "endpoint":"162.159.193.10:2408"
                     }
-                ]
+                ],
+                "reserved":[78, 135, 76],
+                "mtu":1280
             }
         }
     ],
@@ -225,85 +231,90 @@ generate_config() {
 EOF
 }
 
-generate_arco() {
-  cat > /app/arco.sh << ABC
+generate_argo() {
+  cat > argo.sh << ABC
 #!/usr/bin/env bash
 
-arco_type() {
-  if [[ -n "\${ARCO_AUTH}" && -n "\${ARCO_DOMAIN}" ]]; then
-    [[ \$ARCO_AUTH =~ TunnelSecret ]] && echo \$ARCO_AUTH > /app/arco.json && cat > /app/arco.yml << EOF
-tunnel: \$(cut -d\" -f12 <<< \$ARCO_AUTH)
-credentials-file: /app/arco.json
-protocol: h2mux
+argo_type() {
+  if [[ -n "\${ARGO_AUTH}" && -n "\${ARGO_DOMAIN}" ]]; then
+    [[ \$ARGO_AUTH =~ TunnelSecret ]] && echo \$ARGO_AUTH > tunnel.json && cat > tunnel.yml << EOF
+tunnel: \$(cut -d\" -f12 <<< \$ARGO_AUTH)
+credentials-file: /app/tunnel.json
+protocol: http2
 
 ingress:
-  - hostname: \$ARCO_DOMAIN
+  - hostname: \$ARGO_DOMAIN
     service: http://localhost:8080
 EOF
 
-    [ -n "\${SSH_DOMAIN}" ] && cat >> /app/arco.yml << EOF
+    [ -n "\${SSH_DOMAIN}" ] && cat >> tunnel.yml << EOF
   - hostname: \$SSH_DOMAIN
     service: http://localhost:2222
 EOF
 
-    cat >> /app/arco.yml << EOF
+    [ -n "\${FTP_DOMAIN}" ] && cat >> tunnel.yml << EOF
+  - hostname: \$FTP_DOMAIN
+    service: http://localhost:3333
+EOF
+
+    cat >> tunnel.yml << EOF
     originRequest:
       noTLSVerify: true
   - service: http_status:404
 EOF
 
   else
-    ARCO_DOMAIN=\$(cat /app/arco.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
+    ARGO_DOMAIN=\$(cat argo.log | grep -o "info.*https://.*trycloudflare.com" | sed "s@.*https://@@g" | tail -n 1)
   fi
 }
 
 export_list() {
-  VMESS="{ \"v\": \"2\", \"ps\": \"Arco-Vmess\", \"add\": \"icook.hk\", \"port\": \"443\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"\${ARCO_DOMAIN}\", \"path\": \"/${WSPATH}-vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"\${ARCO_DOMAIN}\", \"alpn\": \"\" }"
+  VMESS="{ \"v\": \"2\", \"ps\": \"Argo-Vmess\", \"add\": \"icook.hk\", \"port\": \"443\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"\${ARGO_DOMAIN}\", \"path\": \"/${WSPATH}-vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"\${ARGO_DOMAIN}\", \"alpn\": \"\" }"
 
-  cat > /app/list << EOF
+  cat > list << EOF
 *******************************************
 V2-rayN:
 ----------------------------
-vless://${UUID}@icook.hk:443?encryption=none&security=tls&sni=\${ARCO_DOMAIN}&type=ws&host=\${ARCO_DOMAIN}&path=%2F${WSPATH}-vless?ed=2048#Arco-Vless
+vless://${UUID}@icook.hk:443?encryption=none&security=tls&sni=\${ARGO_DOMAIN}&type=ws&host=\${ARGO_DOMAIN}&path=%2F${WSPATH}-vless?ed=2048#Argo-Vless
 ----------------------------
 vmess://\$(echo \$VMESS | base64 -w0)
 ----------------------------
-trojan://${UUID}@icook.hk:443?security=tls&sni=\${ARCO_DOMAIN}&type=ws&host=\${ARCO_DOMAIN}&path=%2F${WSPATH}-trojan?ed=2048#Arco-Trojan
+trojan://${UUID}@icook.hk:443?security=tls&sni=\${ARGO_DOMAIN}&type=ws&host=\${ARGO_DOMAIN}&path=%2F${WSPATH}-trojan?ed=2048#Argo-Trojan
 ----------------------------
-ss://$(echo "chacha20-ietf-poly1305:${UUID}@icook.hk:443" | base64 -w0)@icook.hk:443#Arco-Shadowsocks
-由于该软件导出的链接不全，请自行处理如下: 传输协议: WS ， 伪装域名: \${ARCO_DOMAIN} ，路径: /${WSPATH}-shadowsocks?ed=2048 ， 传输层安全: tls ， sni: \${ARCO_DOMAIN}
+ss://$(echo "chacha20-ietf-poly1305:${UUID}@icook.hk:443" | base64 -w0)@icook.hk:443#Argo-Shadowsocks
+由于该软件导出的链接不全，请自行处理如下: 传输协议: WS ， 伪装域名: \${ARGO_DOMAIN} ，路径: /${WSPATH}-shadowsocks?ed=2048 ， 传输层安全: tls ， sni: \${ARGO_DOMAIN}
 *******************************************
 小火箭:
 ----------------------------
-vless://${UUID}@icook.hk:443?encryption=none&security=tls&type=ws&host=\${ARCO_DOMAIN}&path=/${WSPATH}-vless?ed=2048&sni=\${ARCO_DOMAIN}#Arco-Vless
+vless://${UUID}@icook.hk:443?encryption=none&security=tls&type=ws&host=\${ARGO_DOMAIN}&path=/${WSPATH}-vless?ed=2048&sni=\${ARGO_DOMAIN}#Argo-Vless
 ----------------------------
-vmess://$(echo "none:${UUID}@icook.hk:443" | base64 -w0)?remarks=Arco-Vmess&obfsParam=\${ARCO_DOMAIN}&path=/${WSPATH}-vmess?ed=2048&obfs=websocket&tls=1&peer=\${ARCO_DOMAIN}&alterId=0
+vmess://$(echo "none:${UUID}@icook.hk:443" | base64 -w0)?remarks=Argo-Vmess&obfsParam=\${ARGO_DOMAIN}&path=/${WSPATH}-vmess?ed=2048&obfs=websocket&tls=1&peer=\${ARGO_DOMAIN}&alterId=0
 ----------------------------
-trojan://${UUID}@icook.hk:443?peer=\${ARCO_DOMAIN}&plugin=obfs-local;obfs=websocket;obfs-host=\${ARCO_DOMAIN};obfs-uri=/${WSPATH}-trojan?ed=2048#Arco-Trojan
+trojan://${UUID}@icook.hk:443?peer=\${ARGO_DOMAIN}&plugin=obfs-local;obfs=websocket;obfs-host=\${ARGO_DOMAIN};obfs-uri=/${WSPATH}-trojan?ed=2048#Argo-Trojan
 ----------------------------
-ss://$(echo "chacha20-ietf-poly1305:${UUID}@icook.hk:443" | base64 -w0)?obfs=wss&obfsParam=\${ARCO_DOMAIN}&path=/${WSPATH}-shadowsocks?ed=2048#Arco-Shadowsocks
+ss://$(echo "chacha20-ietf-poly1305:${UUID}@icook.hk:443" | base64 -w0)?obfs=wss&obfsParam=\${ARGO_DOMAIN}&path=/${WSPATH}-shadowsocks?ed=2048#Argo-Shadowsocks
 *******************************************
 Clash:
 ----------------------------
-- {name: Arco-Vless, type: vless, server: icook.hk, port: 443, uuid: ${UUID}, tls: true, servername: \${ARCO_DOMAIN}, skip-cert-verify: false, network: ws, ws-opts: {path: /${WSPATH}-vless?ed=2048, headers: { Host: \${ARCO_DOMAIN}}}, udp: true}
+- {name: Argo-Vless, type: vless, server: icook.hk, port: 443, uuid: ${UUID}, tls: true, servername: \${ARGO_DOMAIN}, skip-cert-verify: false, network: ws, ws-opts: {path: /${WSPATH}-vless?ed=2048, headers: { Host: \${ARGO_DOMAIN}}}, udp: true}
 ----------------------------
-- {name: Arco-Vmess, type: vmess, server: icook.hk, port: 443, uuid: ${UUID}, alterId: 0, cipher: none, tls: true, skip-cert-verify: true, network: ws, ws-opts: {path: /${WSPATH}-vmess?ed=2048, headers: {Host: \${ARCO_DOMAIN}}}, udp: true}
+- {name: Argo-Vmess, type: vmess, server: icook.hk, port: 443, uuid: ${UUID}, alterId: 0, cipher: none, tls: true, skip-cert-verify: true, network: ws, ws-opts: {path: /${WSPATH}-vmess?ed=2048, headers: {Host: \${ARGO_DOMAIN}}}, udp: true}
 ----------------------------
-- {name: Arco-Trojan, type: trojan, server: icook.hk, port: 443, password: ${UUID}, udp: true, tls: true, sni: \${ARCO_DOMAIN}, skip-cert-verify: false, network: ws, ws-opts: { path: /${WSPATH}-trojan?ed=2048, headers: { Host: \${ARCO_DOMAIN} } } }
+- {name: Argo-Trojan, type: trojan, server: icook.hk, port: 443, password: ${UUID}, udp: true, tls: true, sni: \${ARGO_DOMAIN}, skip-cert-verify: false, network: ws, ws-opts: { path: /${WSPATH}-trojan?ed=2048, headers: { Host: \${ARGO_DOMAIN} } } }
 ----------------------------
-- {name: Arco-Shadowsocks, type: ss, server: icook.hk, port: 443, cipher: chacha20-ietf-poly1305, password: ${UUID}, plugin: v2ray-plugin, plugin-opts: { mode: websocket, host: \${ARCO_DOMAIN}, path: /${WSPATH}-shadowsocks?ed=2048, tls: true, skip-cert-verify: false, mux: false } }
+- {name: Argo-Shadowsocks, type: ss, server: icook.hk, port: 443, cipher: chacha20-ietf-poly1305, password: ${UUID}, plugin: v2ray-plugin, plugin-opts: { mode: websocket, host: \${ARGO_DOMAIN}, path: /${WSPATH}-shadowsocks?ed=2048, tls: true, skip-cert-verify: false, mux: false } }
 *******************************************
 EOF
-  cat /app/list
+  cat list
 }
 
-arco_type
+argo_type
 export_list
 ABC
 }
 
 generate_nezha() {
-  cat > /app/nezha.sh << EOF
+  cat > nezha.sh << EOF
 #!/usr/bin/env bash
 
 # 检测是否已运行
@@ -311,19 +322,19 @@ check_run() {
   [[ \$(pgrep -lafx nezha-agent) ]] && echo "哪吒客户端正在运行中" && exit
 }
 
-# 三个变量不全则不安装哪吒客户端
+# 若哪吒三个变量不全，则不安装哪吒客户端
 check_variable() {
   [[ -z "\${NEZHA_SERVER}" || -z "\${NEZHA_PORT}" || -z "\${NEZHA_KEY}" ]] && exit
 }
 
 # 下载最新版本 Nezha Agent
 download_agent() {
-  if [ ! -e /app/nezha-agent ]; then
+  if [ ! -e nezha-agent ]; then
     URL=\$(wget -qO- "https://api.github.com/repos/nezhahq/agent/releases/latest" | grep -o "https.*linux_amd64.zip")
     URL=\${URL:-https://github.com/nezhahq/agent/releases/download/v0.15.6/nezha-agent_linux_amd64.zip}
-    wget -P /app/ \${URL}
-    unzip -qod /app/ /app/nezha-agent_linux_amd64.zip
-    rm -f /app/nezha-agent_linux_amd64.zip
+    wget \${URL}
+    unzip -qod ./ nezha-agent_linux_amd64.zip
+    rm -f nezha-agent_linux_amd64.zip
   fi
 }
 
@@ -334,7 +345,7 @@ EOF
 }
 
 generate_ttyd() {
-  cat > /app/ttyd.sh << EOF
+  cat > ttyd.sh << EOF
 #!/usr/bin/env bash
 
 # 检测是否已运行
@@ -342,18 +353,18 @@ check_run() {
   [[ \$(pgrep -lafx ttyd) ]] && echo "ttyd 正在运行中" && exit
 }
 
-# ssh arco 域名不设置，则不安装 ttyd 服务端
+# 若 ssh argo 域名不设置，则不安装 ttyd
 check_variable() {
   [ -z "\${SSH_DOMAIN}" ] && exit
 }
 
 # 下载最新版本 ttyd
 download_ttyd() {
-  if [ ! -e /app/ttyd ]; then
+  if [ ! -e ttyd ]; then
     URL=\$(wget -qO- "https://api.github.com/repos/tsl0922/ttyd/releases/latest" | grep -o "https.*x86_64")
     URL=\${URL:-https://github.com/tsl0922/ttyd/releases/download/1.7.3/ttyd.x86_64}
-    wget -O /app/ttyd \${URL}
-    chmod +x /app/ttyd
+    wget -O ttyd \${URL}
+    chmod +x ttyd
   fi
 }
 
@@ -363,46 +374,99 @@ download_ttyd
 EOF
 }
 
+generate_filebrowser () {
+  cat > filebrowser.sh << EOF
+#!/usr/bin/env bash
+
+# 检测是否已运行
+check_run() {
+  [[ \$(pgrep -lafx filebrowser) ]] && echo "filebrowser 正在运行中" && exit
+}
+
+# 若 ftp argo 域名不设置，则不安装 filebrowser
+check_variable() {
+  [ -z "\${FTP_DOMAIN}" ] && exit
+}
+
+# 下载最新版本 filebrowser
+download_filebrowser() {
+  if [ ! -e filebrowser ]; then
+    URL=\$(wget -qO- "https://api.github.com/repos/filebrowser/filebrowser/releases/latest" | grep -o "https.*linux-amd64.*gz")
+    URL=\${URL:-https://github.com/filebrowser/filebrowser/releases/download/v2.23.0/linux-amd64-filebrowser.tar.gz}
+    wget -O filebrowser.tar.gz \${URL}
+    tar xzvf filebrowser.tar.gz filebrowser
+    rm -f filebrowser.tar.gz
+    chmod +x filebrowser
+    PASSWORD_HASH=\$(./filebrowser hash \$WEB_PASSWORD)
+    sed -i "s#PASSWORD_HASH#\$PASSWORD_HASH#g" ecosystem.config.js
+  fi
+}
+
+check_run
+check_variable
+download_filebrowser
+EOF
+}
+
+# 生成 pm2 配置文件
 generate_pm2_file() {
-  if [[ -n "${ARCO_AUTH}" && -n "${ARCO_DOMAIN}" ]]; then
-    [[ $ARCO_AUTH =~ TunnelSecret ]] && ARCO_ARGS="tunnel --edge-ip-version auto --config /app/arco.yml run"
-    [[ $ARCO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]] && ARCO_ARGS="tunnel --edge-ip-version auto --protocol h2mux run --token ${ARCO_AUTH}"
+  if [[ -n "${ARGO_AUTH}" && -n "${ARGO_DOMAIN}" ]]; then
+    [[ $ARGO_AUTH =~ TunnelSecret ]] && ARGO_ARGS="tunnel --edge-ip-version auto --config tunnel.yml run"
+    [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]] && ARGO_ARGS="tunnel --edge-ip-version auto --protocol http2 run --token ${ARGO_AUTH}"
   else
-    ARCO_ARGS="tunnel --edge-ip-version auto --protocol h2mux --no-autoupdate --logfile /app/arco.log --loglevel info --url http://localhost:8080"
+    ARGO_ARGS="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile argo.log --loglevel info --url http://localhost:8080"
   fi
 
   TLS=${NEZHA_TLS:+'--tls'}
 
-  cat > /app/ecosystem.config.js << EOF
+  cat > ecosystem.config.js << EOF
 module.exports = {
   "apps":[
       {
-          "name":"web",
-          "script":"/app/web.js run"
+          name: 'web',
+          script: '/app/web.js run',
+          out_file: "/dev/null",
+          error_file: "/dev/null"
       },
       {
-          "name":"arco",
-          "script":"cloudflared",
-          "args":"${ARCO_ARGS}"
+          name: 'argo',
+          script: 'cloudflared',
+          args: "${ARGO_ARGS}",
+          out_file: "/dev/null",
+          error_file: "/dev/null"
 EOF
 
-  [[ -n "${NEZHA_SERVER}" && -n "${NEZHA_PORT}" && -n "${NEZHA_KEY}" ]] && cat >> /app/ecosystem.config.js << EOF
+  [[ -n "${NEZHA_SERVER}" && -n "${NEZHA_PORT}" && -n "${NEZHA_KEY}" ]] && cat >> ecosystem.config.js << EOF
       },
       {
-          "name":"nezha",
-          "script":"/app/nezha-agent",
-          "args":"-s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${TLS}"
-EOF
-  
-  [ -n "${SSH_DOMAIN}" ] && cat >> /app/ecosystem.config.js << EOF
-      },
-      {
-          "name":"ttyd",
-          "script":"/app/ttyd",
-          "args":"-c ${WEB_USERNAME}:${WEB_PASSWORD} -p 2222 bash"
+          name: 'nezha',
+          script: '/app/nezha-agent',
+          args: "-s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${TLS}",
+          out_file: "/dev/null",
+          error_file: "/dev/null"
 EOF
 
-  cat >> /app/ecosystem.config.js << EOF
+  [ -n "${SSH_DOMAIN}" ] && cat >> ecosystem.config.js << EOF
+      },
+      {
+          name: 'ttyd',
+          script: '/app/ttyd',
+          args: "-c ${WEB_USERNAME}:${WEB_PASSWORD} -p 2222 bash",
+          out_file: "/dev/null",
+          error_file: "/dev/null"
+EOF
+
+  [ -n "${FTP_DOMAIN}" ] && cat >> ecosystem.config.js << EOF
+      },
+      {
+          name: 'filebrowser',
+          script: '/app/filebrowser',
+          args: "--port 3333 --username ${WEB_USERNAME} --password 'PASSWORD_HASH'",
+          out_file: "/dev/null",
+          error_file: "/dev/null"
+EOF
+
+  cat >> ecosystem.config.js << EOF
       }
   ]
 }
@@ -410,12 +474,14 @@ EOF
 }
 
 generate_config
-generate_arco
+generate_argo
 generate_nezha
 generate_ttyd
+generate_filebrowser
 generate_pm2_file
 
-[ -e /app/nezha.sh ] && bash /app/nezha.sh
-[ -e /app/arco.sh ] && bash /app/arco.sh
-[ -e /app/ttyd.sh ] && bash /app/ttyd.sh
-[ -e /app/ecosystem.config.js ] && pm2 start /app/ecosystem.config.js
+[ -e nezha.sh ] && bash nezha.sh
+[ -e argo.sh ] && bash argo.sh
+[ -e ttyd.sh ] && bash ttyd.sh
+[ -e filebrowser.sh ] && bash filebrowser.sh
+[ -e ecosystem.config.js ] && pm2 start
